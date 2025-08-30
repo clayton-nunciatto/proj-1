301f75697049bdbdc9c60086f8498249e1786a16
clayton-nunciatto <clayton.nunciatto@gmail.com>
2025-08-30 15:49:25 -0300

    Primeira Atividade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,11 @@ package org.example;
 
 
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Olá, Mundo!");
 
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
     }
+
 }
